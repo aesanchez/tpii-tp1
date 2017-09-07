98fe924e632c26701f6c6f0d6c9e5605a614728d
nternouski <nahuelternouski@gmail.com>
2017-09-07 19:30:56 -0300

    sin prints y utils

--- punto-3/thread.py
+++ punto-3/thread.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.insert(0, './util')
 import threading
 import time
 import os.path
@@ -8,26 +9,16 @@ from filemutex import mutexV
 
 def worker(count, t_muestreo, filename):
     cant_muestras = 1
-    p_temp = 0
-    p_humedad = 0
-    p_presion = 0
-    p_viento = 0
-    """La a es para que se escriba al final del archivo"""
     while cant_muestras < 4:
         temp = randint(0, 90)
         humedad = randint(0, 90)
         presion = randint(0, 90)
         v_viento = randint (0, 90)
-        p_temp = (p_temp + temp)/cant_muestras
-        p_humedad = (p_humedad + humedad)/cant_muestras
-        p_presion = (p_presion + presion)/cant_muestras
-        p_viento = (p_viento + v_viento)/cant_muestras
         time.sleep(t_muestreo)
         cant_muestras = cant_muestras + 1
         mutexP(filename)
         file = open(filename, "a")
         time.sleep(2)
-        print "me duermo (t={}) {} seg .. promedio temp = {}".format(count, t_muestreo, p_temp)
         file.write("{}-{}|{}|{}|{}\n".format(count, temp,humedad,presion,v_viento))
         file.close()
         mutexV(filename)
@@ -35,8 +26,7 @@ def worker(count, t_muestreo, filename):
 
 threads = list()
 
-for i in range(3):
-    print sys.argv[1]
+for i in range(1):
     t = threading.Thread(target=worker, args=(i, 0.001, sys.argv[1],))
     threads.append(t)
     t.start()
