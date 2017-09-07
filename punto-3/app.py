f0424d3560bba0784f3fad3d7dc4bc19e2a68c91
Agustin Sanchez <trorik23@gmail.com>
2017-09-07 15:29:01 -0300

    simple suma usando lo de la practica

--- punto-3/app.py
+++ punto-3/app.py
@@ -8,9 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/result', methods = ['POST'])
+def result():
+    #vuelvo a comprobar
+    if request.method =='POST':
+        data = request.form
+        var1 = data["var1"]
+        var2 = data["var2"]
+        result = int(var1) + int(var2)
+        return render_template('result.html', result = result)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=80)
