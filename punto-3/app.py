e2879eae7bcb0dd821992ba85a9096230f93e081
Agustin Sanchez <trorik23@gmail.com>
2017-09-07 18:35:29 -0300

    merge

--- punto-3/app.py
+++ punto-3/app.py
@@ -1,22 +1,24 @@
+# Aqui se listan los imports
 from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
+# Define la ruta con la que se ingresara desde el browser
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('form.html')
 
-@app.route('/result', methods = ['POST'])
-def result():
-    #vuelvo a comprobar
-    if request.method =='POST':
+# Define la ruta y metodo con el que se debe llegar a este endpoint
+@app.route('/form', methods = ['POST'])
+def action_form():
+
+    if request.method == 'POST':
         data = request.form
-        var1 = data["var1"]
-        var2 = data["var2"]
-        result = int(var1) + int(var2)
-        return render_template('result.html', result = result)
+        nombre = data["nombre"]
+        return render_template('datos.html', nombre=nombre)
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=80)
+    # Define HOST y PUERTO para accerder
+    app.run(host='localhost', port=86) 
---
+++ punto-3/thread.py
@@ -0,0 +1,42 @@
+import sys
+import threading
+import time
+import os.path
+from random import randint
+from filemutex import mutexP
+from filemutex import mutexV
+
+def worker(count, t_muestreo, filename):
+    cant_muestras = 1
+    p_temp = 0
+    p_humedad = 0
+    p_presion = 0
+    p_viento = 0
+    """La a es para que se escriba al final del archivo"""
+    while cant_muestras < 4:
+        temp = randint(0, 90)
+        humedad = randint(0, 90)
+        presion = randint(0, 90)
+        v_viento = randint (0, 90)
+        p_temp = (p_temp + temp)/cant_muestras
+        p_humedad = (p_humedad + humedad)/cant_muestras
+        p_presion = (p_presion + presion)/cant_muestras
+        p_viento = (p_viento + v_viento)/cant_muestras
+        time.sleep(t_muestreo)
+        cant_muestras = cant_muestras + 1
+        mutexP(filename)
+        file = open(filename, "a")
+        time.sleep(2)
+        print "me duermo (t={}) {} seg .. promedio temp = {}".format(count, t_muestreo, p_temp)
+        file.write("{}-{}|{}|{}|{}\n".format(count, temp,humedad,presion,v_viento))
+        file.close()
+        mutexV(filename)
+    return
+
+threads = list()
+
+for i in range(3):
+    print sys.argv[1]
+    t = threading.Thread(target=worker, args=(i, 0.001, sys.argv[1],))
+    threads.append(t)
+    t.start()
